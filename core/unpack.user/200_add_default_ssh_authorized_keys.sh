31344f4c7cfd63ab200bdbd3cfac2c6a71b8d43a
mk-pmb <mk-pmb@users.noreply.github.com>
2026-01-24 21:56:33 +0100

    Refactor setup as runparts scripts: rere_add_default_ssh_authorized_keys

---
+++ core/unpack.user/200_add_default_ssh_authorized_keys.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+set -e
+echo D: 'Ensuring default SSH keys:'
+CFG_AK="$HOME"/.config/ssh
+mkdir --parents -- "$CFG_AK" || true
+[ -L "$HOME"/.ssh ] || ln --symbolic --no-target-directory \
+  -- .config/ssh "$HOME"/.ssh || exit $?$(
+  echo E: 'Failed to create ~/.ssh symlink!' >&2)
+CFG_AK+='/authorized_keys'
+>>"$CFG_AK" || exit $?$(echo E: 'Failed to touch $CFG_AK!' >&2)
+( LANG=C sed -re 's~^\xEF\xBB\xBF~~;s~\r~~' -- \
+  cfg.@.defaults/ssh_authorized_keys.txt \
+  2>/dev/null | grep -vFf "$CFG_AK" || true
+) >>"$CFG_AK" || exit $?$(echo E: 'Failed to update $CFG_AK!' >&2)
