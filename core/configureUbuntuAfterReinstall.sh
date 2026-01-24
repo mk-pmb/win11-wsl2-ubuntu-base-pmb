21d3395edbaab8cc29a1ea0d1072b38fb7911c4c
mk-pmb <mk-pmb@users.noreply.github.com>
2026-01-24 21:35:21 +0100

    Refactor setup as runparts scripts: rere_ensure_apt_pkg

--- core/configureUbuntuAfterReinstall.sh
+++ core/configureUbuntuAfterReinstall.sh
@@ -56,7 +56,6 @@ function rere_detect_windir () {
 function rere_unpack_as_root () {
   ./core/runParts.sh core/unpack.root 'Unpack root parts' || return $?
 
-  rere_ensure_apt_pkg || return $?
   rere_set_default_locale || return $?
 }
 
@@ -88,55 +87,6 @@ function rere_unpack_as_user () {
   echo
   echo D: 'Post-install configuration completed successfully.'
   # debian_chroot='reinstalled' bash -i
-}
-
-
-function rere_ensure_apt_pkg () {
-  echo -n D: 'Check basic apt packages to install: '
-  local LIST=(
-    aptitude
-    fuse3 # would otherwise be uninstalled when purging snapd.
-    nano
-    openssh-server
-    pv
-    screen
-    squashfs-tools # would otherwise be uninstalled when purging snapd.
-    unzip
-    zip
-    )
-  local ITEM= TODO= APT="env debian_frontend='noninteractive' apt"
-  for ITEM in "${LIST[@]}"; do
-    [ -f "/usr/share/doc/$ITEM/copyright" ] ||
-      TODO+=" ${ITEM%%:*}"
-  done
-  if [ -z "$TODO" ]; then
-    echo 'None missing.'
-  else
-    echo 'Some packages are missing.'
-    echo 'D: Update apt package lists:'
-    $APT update || return $?
-    echo 'D: Install missing apt packages:'
-    $APT install --assume-yes -- $TODO || return $?
-    echo 'D: Packages have been installed.'
-  fi
-
-  echo -n D: 'Check basic apt packages to remove: '
-  LIST=(
-    snapd
-    # nope, would uninstall sudo. -> # ubuntu-pro-client{,-l10n}
-    )
-  TODO=
-  for ITEM in "${LIST[@]}"; do
-    [ -f "/usr/share/doc/$ITEM/copyright" ] || continue
-    TODO+=" $ITEM"
-  done
-  if [ -z "$TODO" ]; then
-    echo 'Found none.'
-  else
-    echo 'Found some.'
-    ${APT}itude purge --assume-yes -- $TODO || return $?
-    echo 'D: Packages have been purged.'
-  fi
 }
 
 
