234e0ac32a8e2942dac64838486b6ef835a9431e
mk-pmb <mk-pmb@users.noreply.github.com>
2026-01-24 12:46:37 +0100

    Set default locale.

--- core/configureUbuntuAfterReinstall.sh
+++ core/configureUbuntuAfterReinstall.sh
@@ -67,6 +67,7 @@ function rere_unpack_as_root () {
   systemctl stop wsl-pro.service || return $?
 
   rere_ensure_apt_pkg || return $?
+  rere_set_default_locale || return $?
 }
 
 
@@ -211,6 +212,19 @@ function rere_ensure_keepalive () {
     lnkFile '@:\Startup\Start WSL2 Ubuntu.lnk' icon 'pifmgr.dll,32' \
     winStyle min saveLnk || return $?
   "$PROG" $ARGS || return $?
+}
+
+
+function rere_set_default_locale () {
+  local WIN_LANG="$(powershell.exe Get-WinUserLanguageList |
+    sed -nre 's~\r$~~; s!-!_!; s~^LanguageTag\s+:\s+~~p')"
+  WIN_LANG="${WIN_LANG%%$'\n'*}"
+  case "$WIN_LANG" in
+    [a-z][a-z]_[A-Z][A-Z]* ) ;;
+    * ) WIN_LANG='en_US';;
+  esac
+  printf -- '%s="%s.UTF-8"\n' LANG en_US LC_TIME "$WIN_LANG" \
+    >/etc/default/locale || return $?
 }
 
 
