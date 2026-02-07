c35b6e12aee4fae127981d10124ac4f3b677ebba
mk-pmb <mk-pmb@users.noreply.github.com>
2026-02-07 21:16:55 +0100

    Prevent syslog spam from wsl-pro.service: Skip if not installed.

--- core/unpack.root/400_avoid_syslog_spam.sh
+++ core/unpack.root/400_avoid_syslog_spam.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # -*- coding: utf-8, tab-width: 2 -*-
 set -e
+set -- wsl-pro.service
 
-echo D: "Prevent syslog spam from the wsl-pro-service daemon:"
+echo D: "Prevent syslog spam from the $1 daemon:"
 # https://github.com/microsoft/WSL/issues/12992
-systemctl disable wsl-pro.service
-systemctl stop wsl-pro.service
+
+if ! systemctl cat sshd2.service &>/dev/null; then
+  echo D: "Seems to not be installed => ignore."
+else
+  systemctl disable $1
+  systemctl stop $1
+fi
