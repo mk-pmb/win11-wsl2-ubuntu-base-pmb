ee917240b429f44b62cf32f8288c885c0113ddc4
mk-pmb <mk-pmb@users.noreply.github.com>
2026-08-28 02:25:33 +0200

    [docs] Give a short preview of why we use a bogus password hash.

--- core/unpack.root/100_disable_my_password.sh
+++ core/unpack.root/100_disable_my_password.sh
@@ -4,6 +4,8 @@ set -e
 
 # For why we use a bogus hash instead of locking the account, see chapter
 # "The problem" in https://github.com/mk-pmb/ansible-bogus-linux-pwhash .
+# (Most important benefit: You can use all regular password lock mechanisms
+# for locking, and later unlock them with some custom hardened ceremony.)
 
 BOGUS_PWHASH='$6$fakesalt$::::::::::::=='
 BOGUS_PWHASH="${BOGUS_PWHASH//:/==bogus}"
